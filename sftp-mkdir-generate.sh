a6626a71cfb155585e47f609c088f8b0e884db18
hothoony <hothoony@hanmail.net>
2022-07-25 12:05:53 +0900

    .

---
+++ sftp-mkdir-generate.sh
@@ -0,0 +1,22 @@
+path=/ztemp2/data/skin/front/on_furniterior_r/board
+echo ""
+echo $path
+
+## path 를 디렉토리 뎁스별로 나누고 list 배열에 담기
+list=()
+temp=$path
+while [ ! -z $temp ] && [ $temp != "/" ]
+do
+    list+=($temp)
+    temp=`dirname $temp`
+done
+
+## list 배열을 역순으로 출력
+echo ""
+len=${#list[@]}
+let "i = $len - 1"
+while [ $i -ge 0 ]
+do
+    echo "mkdir" ${list[$i]}
+    let "i--"
+done
